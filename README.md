be143a7647169206917dd1b87afa7bb821b6918d
Mikołaj Skałka <mikolaj.skalka@student.ujedu.pl>
2021-02-23 15:36:44 +0100

    Update a_star and README

--- README.md
+++ README.md
@@ -1,1 +1,79 @@
 # A_star_algorithm
+
+
+### Wprowadzenie:
+
+Plik a_star.py zawiera implementację algorytmu A* oraz funkcjonalności pozwalające na wizualizację jego działania.
+
+Program korzysta z modułu pygame, który jest niezbędny do jego uruchomienia.
+
+### Opis algorytmu:
+
+Serce programu to sam algorytm A*, który służy do znajdowania najkrótszej scieżki pomiędzy dwoma punktami.
+Algorytm wybiera scieżkę spośród nieodwiedzonych jeszcze wierzchołków tak aby minimalizować funkcję f(x), która jest 
+zadana wzorem:
+f(x) = g(x) + h(x)
+gdzie x jest wierzchołkiem sąsiadującym z aktualnie odwiedzanym wierzchołkiem.
+g(x) to funkcja, która oblicza odległość/koszt drogi od punktu startowego do wierzchołka x
+h(x) to funkcja heurystyczna, która oblicza odległość/koszt drogi od wierzchołka x do wierzchołka docelowego.
+
+W tym przypadku funkcja heurystyczna została zaimplementowana jako "Manhatan distance". To znaczy odległość pomiędzy dwoma 
+punktami obliczona jest jako suma bezwzględnych różnic odpowiadających wzspółrzędnych:
+'
+def h(p1, p2): 
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
+' 
+
+Funkcja g(x) w tym przypadku dla każdego kolejnego odwiedzonego wierzchołka będzie wynosić tyle co dla poprzedniego 
+wierzchołka + 1.
+
+
+
+`
+def a_star(draw, start, grid, goal):
+    """ A* path finding algorithm """
+    count = 0
+    openSet = PriorityQueue()
+    openSet.put((0, count, start))
+    cameFrom = {}
+    gScore = {node : float("inf") for row in grid for node in row}
+    gScore[start] = 0  
+    fScore = {node : float("inf") for row in grid for node in row}
+    fScore[start] = h(start.get_pos(), goal.get_pos())
+
+    hashSet = {start}
+
+    while not openSet.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = openSet.get()[2]
+        hashSet.remove(current)
+
+
+        if current == goal:
+            reconstructPath(cameFrom, current, draw)
+            goal.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            tmp_gScore = gScore[current] + 1
+
+            if tmp_gScore < gScore[neighbor]:
+                cameFrom[neighbor] = current
+                gScore[neighbor] = tmp_gScore
+                fScore[neighbor] = gScore[neighbor] + h(neighbor.get_pos(), goal.get_pos())
+                if neighbor not in hashSet:
+                    count += 1
+                    openSet.put((fScore[neighbor], count, neighbor))
+                    hashSet.add(neighbor)
+                    neighbor.make_open()
+        draw()
+        if current != start:
+            current.make_closed()
+    
+    return False
+`
--- a_star.py
+++ a_star.py
@@ -1,5 +1,7 @@
 from queue import PriorityQueue
+import math
 import pygame
+import sys
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -9,11 +11,11 @@ BLUE = (0, 255, 0)
 GREEN = (0, 0, 255)
 ORANGE = (255, 165 ,0)
 GREY = (127, 127, 127)
+PURPLE = (128, 0, 128)
+TURQUOISE = (64, 224, 208)
 
 
 WIDTH = 800
-
-
 
 WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Path Finding Algorithm")
@@ -27,14 +29,69 @@ class Node:
         self.row = row
         self.col = col
         self.x = row * width
-        self.y = row * width
-        self.color  = WHITE
+        self.y = col * width
+        self.color = WHITE
         self.neighbors = []
         self.width = width
         self.total_rows = total_rows
 
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.width))
+
+    def get_pos(self):
+        return self.row, self.col
+
+    def is_wall(self):
+        return self.color == BLACK
+
+    def is_visited(self):
+        return self.color == RED
+
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_start(self):
+        return self.color == ORANGE
+
+    def is_end(self):
+        return self.color == TURQUOISE
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_wall(self):
+        self.color = BLACK
+    
+    def reset(self):
+        self.color = WHITE
+
+    def make_path(self):
+        self.color = PURPLE
+    
+    def make_start(self):
+        self.color = ORANGE
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def update_neighbors(self, grid):
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_wall():
+            self.neighbors.append(grid[self.row + 1][self.col])
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_wall():
+            self.neighbors.append(grid[self.row][self.col + 1])
+        if self.row > 0 and not grid[self.row - 1][self.col].is_wall():
+            self.neighbors.append(grid[self.row - 1][self.col])
+        if self.col > 0 and not grid[self.row][self.col - 1].is_wall():
+            self.neighbors.append(grid[self.row][self.col - 1])
+
+    def __lt__(self, other):
+        return False
+
+
 
 
 # Manhatan distance
@@ -43,12 +100,57 @@ def h(p1, p2):
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
 
-def reconstruckPath(cameFrom, current):
-    pass
+def reconstructPath(cameFrom, current, draw):
+    while current in cameFrom.keys():
+        current = cameFrom[current]
+        current.make_path()
+        draw()
 
-def a_star(start, grid, goal, h):
-    """ A* path finding algorithm to go th """
-    pass 
+def a_star(draw, start, grid, goal):
+    """ A* path finding algorithm """
+    count = 0
+    openSet = PriorityQueue()
+    openSet.put((0, count, start))
+    cameFrom = {}
+    gScore = {node : float("inf") for row in grid for node in row}
+    gScore[start] = 0  
+    fScore = {node : float("inf") for row in grid for node in row}
+    fScore[start] = h(start.get_pos(), goal.get_pos())
+
+    hashSet = {start}
+
+    while not openSet.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = openSet.get()[2]
+        hashSet.remove(current)
+
+
+        if current == goal:
+            reconstructPath(cameFrom, current, draw)
+            goal.make_end()
+            return True
+
+        for neighbor in current.neighbors:
+            tmp_gScore = gScore[current] + 1
+
+            if tmp_gScore < gScore[neighbor]:
+                cameFrom[neighbor] = current
+                gScore[neighbor] = tmp_gScore
+                fScore[neighbor] = gScore[neighbor] + h(neighbor.get_pos(), goal.get_pos())
+                if neighbor not in hashSet:
+                    count += 1
+                    openSet.put((fScore[neighbor], count, neighbor))
+                    hashSet.add(neighbor)
+                    neighbor.make_open()
+        draw()
+        if current != start:
+            current.make_closed()
+    
+    return False
+
 
 
 def make_grid(width, rows):
@@ -57,7 +159,8 @@ def make_grid(width, rows):
     for i in range(rows):
         grid.append([])
         for j in range(rows):
-            grid[i].append(Node(i, j, gap, rows))
+            node = Node(i, j, gap, rows)
+            grid[i].append(node)
     return grid
 
 def draw_grid(win, rows, width):
@@ -77,19 +180,75 @@ def draw(window, grid, rows, width):
     draw_grid(window, rows, width)
     pygame.display.update()
 
+def get_clicked_node(pos, width, rows):
+    gap = width // rows
+    y, x = pos
+
+    row = y // gap
+    col = x // gap
+
+    return row, col
+
 
 
 def main(window, width):
     ROWS = 50
-    grid = make_grid(ROWS, width)
+    grid = make_grid(width, ROWS)
+
+    start = None
+    stop = None
+
 
     run = True
 
-    while run :
+    while run:
         draw(window, grid, ROWS, width)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_node(pos, width, ROWS)
+                node = grid[row][col]
+                if not start and node != stop:
+                    start = node
+                    start.make_start()
+                elif not stop and node != start:
+                    stop = node
+                    stop.make_end()
+                elif node != start and node != stop:
+                    node.make_wall()
+                
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_node(pos, width, ROWS)
+                node = grid[row][col]
+                if node == start:
+                    start = None
+                elif node == stop:
+                    stop = None
+                else:
+                    node.reset()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and stop:
+                    for row in grid:
+                        for node in row:
+                            node.update_neighbors(grid)
+                
+                    a_star(lambda: draw(window, grid, ROWS, width), start, grid, stop)
+                
+                elif event.key == pygame.K_n:
+                    start = None
+                    stop = None
+                    for row in grid:
+                        for node in row:
+                            
+                            node.reset()
+
+    pygame.quit()
+
 
 
 if __name__ == "__main__":
